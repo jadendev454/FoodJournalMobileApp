2e342ef4265ec5da5a59d32e4639dd1ef8efeff3
jaden <cloetejaden@outlook.com>
2022-11-21 09:03:46 +0200

    Initial Commit

---
+++ FoodJournalApp/FoodJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodJournalAppApp.swift
+//  FoodJournalApp
+//
+//  Created by IACD 05 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
